d17d6d8fb50d3976f8ed735ef7ab4057a4145f5d
iamsoumya1611 <soumyamajumder2022@gmail.com>
2025-11-08 00:01:09 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const app = express();
+
+const port = 8080;
+
+app.listen(port, () => {
+  console.log(`server is running on port number ${port}`);
+});
