414e301c5b1bb236f86948f1fd09f90c25c12d50
iamsoumya1611 <soumyamajumder2022@gmail.com>
2025-11-08 14:14:15 +0530

    Refactor Navbar component structure for improved readability

--- client/src/components/header/Navbar.jsx
+++ client/src/components/header/Navbar.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import './navbar.css'
 
 const Navbar = () => {
-  return (
-    <header>
-        <nav>
-            <div className="left">
-                <img src="./amazon_PNG25.png" alt="" />
-            </div>
+    return (
+        <header>
+            <nav>
+                <div className="left">
+                    <div className="navlogo">
+                        <img src="./amazon_PNG25.png" alt="" />
+                    </div>
+                </div>
 
-            <div className="right"></div>
-        </nav>
-    </header>
-  )
+                <div className="right">
+
+                </div>
+            </nav>
+        </header>
+    )
 }
 
 export default Navbar
