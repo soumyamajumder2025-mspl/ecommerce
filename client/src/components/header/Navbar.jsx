35517b41ce0441bd5d8837f254b506e8811fd3e4
iamsoumya1611 <soumyamajumder2022@gmail.com>
2025-11-08 15:59:26 +0530

    feat: enhance Navbar component with shopping cart and user avatar functionality

--- client/src/components/header/Navbar.jsx
+++ client/src/components/header/Navbar.jsx
@@ -1,5 +1,8 @@
 import './navbar.css'
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { Badge } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
 
 const Navbar = () => {
     return (
@@ -20,6 +23,20 @@ const Navbar = () => {
                 </div>
 
                 <div className="right">
+                    <div className="nav_btn">
+                        <a href="">signin</a>
+                    </div>
+
+                    <div className="cart_btn">
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartIcon id="icon" />
+                            <p>Cart</p>
+                        </Badge>
+                    </div>
+
+                    <Avatar className='avtar' />
+
+
 
                 </div>
             </nav>
