c491ef03abbdc35117d5b3a97b237dd47862df73
iamsoumya1611 <soumyamajumder2022@gmail.com>
2025-11-08 13:50:50 +0530

    Add Navbar components and update index.html title and favicon

---
+++ client/src/components/header/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <div className="left">
+                <img src="./amazon_PNG25.png" alt="" />
+            </div>
+
+            <div className="right"></div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ client/src/components/newnavbar/NewNavbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewNavbar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default NewNavbar
