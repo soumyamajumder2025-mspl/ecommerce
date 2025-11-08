74e56d88e6d9c912e7a9e5bbcdc5c1af04c2457b
iamsoumya1611 <soumyamajumder2022@gmail.com>
2025-11-08 16:34:12 +0530

    Add undefsafe package for safe property access and update server dependencies
    - Introduced the undefsafe package to safely retrieve and set deep object properties.
    - Added .travis.yml for CI configuration.
    - Included LICENSE and README.md files for the undefsafe package.
    - Created example.js to demonstrate usage of undefsafe.
    - Implemented the main functionality in lib/undefsafe.js.
    - Updated package.json to include undefsafe as a dependency and added nodemon for development.
    - Updated package-lock.json to reflect new dependencies and versions.

--- client/src/components/newnavbar/NewNavbar.jsx
+++ client/src/components/newnavbar/NewNavbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import './newnavbar.css'
 
 const NewNavbar = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div className="new_nav">
+            <div className="nav_data">
+                <div className="left_data">
+                    <p>All</p>
+                    <p>Mobile</p>
+                    <p>Bestseller</p>
+                    <p>Fashion</p>
+                    <p>Customer Services</p>
+                    <p>Electronics</p>
+                    <p>Prime</p>
+                    <p>Today's deal</p>
+                    <p>Amazon Pay</p>
+                </div>
+
+                <div className="right_data">
+                    <img src="./nav.jpg" alt="navdata" />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default NewNavbar
--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ dotenv.config();
 const express = require('express');
 const app = express();
 
-const port = 8080;
+const port = 5000;
 
 app.listen(port, () => {
-  console.log(`server is running on port number ${port}`);
+  console.log(`server is running on port ${port}`);
 });
